1dc4db4bc380d6e57febd5a8e8f304286013a95d
K4SHMIR <107944505+K4SHMIR@users.noreply.github.com>
2023-06-29 13:04:31 -0300

    Create punto2.java

---
+++ punto2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entero: ");
+        int numero = scanner.nextInt();
+
+        // Verificación si es par o impar
+        if (numero % 2 == 0) {
+            System.out.println("El número ingresado es par.");
+        } else {
+            System.out.println("El número ingresado es impar.");
+        }
+    }
+}
